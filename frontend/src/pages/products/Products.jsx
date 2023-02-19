c07ca914e6e02804a9efbaf636450845dab25b0f
TahaHammouz <t.r.hamooz@gmail.com>
2023-02-19 18:04:13 +0200

    search box for products page

--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { Table } from "antd";
 import { columns } from "./ProductsColumns";
 import Button from "../../components/UI/Button/Button";
 import ProductForm from "./ProductForm";
 import Modal from "../../components/UI/Modal/Modal";
+import { Input } from "antd";
+import CustomTable from "../../components/UI/Table/Table";
+const { Search } = Input;
 const Products = () => {
   const [productList, setProductList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [originalProductList, setOriginalProductList] = useState([]);
   const [error, setError] = useState();
 
+  const handleSearch = (event) => {
+    const value = event.target.value;
+    const filteredList = originalProductList.filter((product) =>
+      product.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setProductList(filteredList);
+  };
+  const handleClearSearch = () => {
+    setProductList(originalProductList);
+  };
   useEffect(() => {
     async function fetchData() {
       setIsLoading(true);
@@ -19,6 +32,7 @@ const Products = () => {
         }
         const resData = await response.json();
         setProductList(resData);
+        setOriginalProductList(resData);
       } catch (error) {
         setError(error.message);
       }
@@ -29,16 +43,23 @@ const Products = () => {
 
   return (
     <>
-    
       <div className="d-flex justify-content-between">
         <h3>Products</h3>
-      
+
         <Button>Add Item</Button>
       </div>
-      <Table
+
+      <Search
+        placeholder="Search by product name"
+        onKeyUp={handleSearch}
+        onSearch={handleSearch}
+        onClear={handleClearSearch}
+        style={{ width: 300, marginBottom: 20 }}
+      />
+      <CustomTable
         columns={columns}
         dataSource={productList}
-        loading={isLoading}
+        isLoading={isLoading}
         rowKey="id"
         pagination={{ pageSize: 4 }}
         bordered
