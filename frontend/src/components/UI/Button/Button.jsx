feb7cf6dd068117c5f034fbfdca0be462c4df9c1
TahaHammouz <t.r.hamooz@gmail.com>
2023-02-19 16:52:05 +0200

    Button component

---
+++ frontend/src/components/UI/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from "./Button.module.css"
+
+const Button = (props) => {
+  return (
+      <button type={props.type || 'button'}
+          onClick={props.onClick}
+          className={styles.button} 
+      >
+          {props.children}
+      </button>
+  )
+}
+
+export default Button
