a9825fb941a5bf9a79525877aeeb2461730ead78
TahaHammouz <t.r.hamooz@gmail.com>
2023-02-24 18:20:28 +0200

    thunk function to fetch product data

--- frontend/src/redux/slices/productSlice.js
+++ frontend/src/redux/slices/productSlice.js
@@ -1,7 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { notification } from "antd";
 
+import { API_BASE_URL } from "../../constants/api";
 const initialState = {
   products: [],
+  loading: false,
 };
 
 const productSlice = createSlice({
@@ -18,9 +22,31 @@ const productSlice = createSlice({
       };
       state.products.push(newProduct);
     },
+    setProductsData: (state, action) => {
+      state.products = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
   },
 });
 
-export const { addProduct } = productSlice.actions;
+export const { addProduct, setLoading,setProductsData } = productSlice.actions;
 
 export default productSlice;
+
+
+export const fetchProducts = () => async (dispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const response = await axios.get(`${API_BASE_URL}/products`);
+    dispatch(setProductsData(response.data));
+  } catch (error) {
+    dispatch(setLoading(false));
+    notification.error({
+      message: `Category adding failed: ${error.message}`,
+    });
+  } finally {
+    dispatch(setLoading(false));
+  }
+};
