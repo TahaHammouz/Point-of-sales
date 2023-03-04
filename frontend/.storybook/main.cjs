0c477612f22c02a82b47fba090af032565f13503
TahaHammouz <t.r.hamooz@gmail.com>
2023-03-04 18:16:48 +0200

    Storybook for category table

--- frontend/.storybook/main.cjs
+++ frontend/.storybook/main.cjs
@@ -1,18 +1,5 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+    stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  };
+  
---
+++ frontend/src/pages/categories/CategoryTable.stories.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import CategoryTable from "./CategoryTable";
+
+const mockStore = configureMockStore();
+
+const categories = [
+  { id: 1, category: "Category 1" },
+  { id: 2, category: "Category 2" },
+  { id: 3, category: "Category 3" },
+];
+
+const storeWithCategories = mockStore({
+  categories: { categories },
+});
+
+const storeWithoutCategories = mockStore({
+  categories: { categories: [] },
+});
+
+storiesOf("CategoryTable", module)
+  .addDecorator((story) => <Provider store={storeWithCategories}>{story()}</Provider>)
+  .add("with categories", () => <CategoryTable />)
+  .addDecorator((story) => <Provider store={storeWithoutCategories}>{story()}</Provider>)
+  .add("without categories", () => <CategoryTable />);
